80ffbb4caa5679a1230e81eeb52eb6d15b0a842e
Juan Diego GF <juan.gomez@maker.com.mx>
2026-08-19 17:17:04 -0600

    Logo en navegador

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" type="image/png" href="{{ asset('img/logo-square.png') }}">
     <title>Iniciar sesión · Control Escolar de Learning Play House</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css" rel="stylesheet">
